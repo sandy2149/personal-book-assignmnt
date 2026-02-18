a5e622b8b27204e5b2c6424c4421aab485522c6c
DESKTOP-UAS6EEJ\Sandeep Chail <sandy4docs@gmail.com>
2026-02-18 21:30:33 +0530

    fixed repositor id type to match Book entity String

--- src/main/java/com/example/demo/db/BookRepository.java
+++ src/main/java/com/example/demo/db/BookRepository.java
@@ -2,5 +2,5 @@ package com.example.demo.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, String> {
 }
