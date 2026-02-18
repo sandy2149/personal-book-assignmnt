cd34a56d05176a968de4b9d0b7f9786f4c31c904
DESKTOP-UAS6EEJ\Sandeep Chail <sandy4docs@gmail.com>
2026-02-18 22:10:31 +0530

    Adding ErrorConstants and CustomeException class

---
+++ src/main/java/com/example/demo/constants/ErrorConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.constants;
+
+public final class ErrorConstants {
+
+    private ErrorConstants() {}
+
+    public static final String BOOK_ALREADY_EXISTS = "Book already exists";
+    public static final String BOOK_NOT_FOUND = "Book not found";
+}
